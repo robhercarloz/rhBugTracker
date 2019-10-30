d850fc852bf76877a2d22b44af635fdbd842a6ed
robhercarloz <rhernandezroberto@Gmail.com>
2019-10-30 19:36:58 -0400

    BugtrackerUpdate

---
+++ rhBugTracker/Controllers/ProjectManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rhBugTracker.Controllers
+{
+    public class ProjectManagerController : Controller
+    {
+        // GET: ProjectManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
